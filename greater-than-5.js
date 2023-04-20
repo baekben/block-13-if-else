2da8a2f0648cd8d222c71386f8e7b5a2d3c41875
Ben Baek <benbaeksea@gmail.com>
2023-04-19 21:50:01 -0700

    adding greater-than-5.js

---
+++ greater-than-5.js
@@ -0,0 +1,10 @@
+// script prints true if both parameters are greater than or equal to 5
+
+let num1 = 5;
+let num2 = 6;
+
+if (num1 >= 5 && num2 >= 5) {
+	console.log(true);
+} else {
+	console.log(false);
+}
